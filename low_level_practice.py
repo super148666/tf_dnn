0c175de94a71ca943c85b43e379fe85a3aefcc0a
ChaoZ <zc21435393@gmail.com>
2018-06-04 09:45:07 +0800

    yeah success model

--- low_level_practice.py
+++ low_level_practice.py
@@ -2,67 +2,166 @@ from __future__ import division, print_function, absolute_import
 
 import tensorflow as tf
 
-from tensorflow.examples.tutorials.mnist import input_data
-
-from random import shuffle
-import glob
+import os
+import random
 
 old_v = tf.logging.get_verbosity()
 tf.logging.set_verbosity(tf.logging.ERROR)
 
-mnist = input_data.read_data_sets("/media/chao/RAID1_L/chaoz/mnist_dataset/", one_hot=True)
+seed = 100
+
+# mnist = input_data.read_data_sets("/media/chao/RAID1_L/chaoz/mnist_dataset/", one_hot=True)
+image_path = ["/Users/apple/dataset/basler32/",
+              "/Users/apple/dataset/basler/",
+              "/Users/apple/dataset/webcam/"]
+
+# Image Parameters
+image_width = 32
+image_height = 32
+image_channel = 3
+
+# Dataset Partition
+train_partition = 0.6
+valid_partition = 0.2
+test_partition = 1.0 - valid_partition - train_partition
 
 # Training Parameters
 learning_rate = 0.001
-num_steps = 500
+num_steps = 5000
 batch_size = 128
 display_step = 10
 
 # Network Parameters
-num_input = 784 # MNIST data input (img shape: 28*28)
-num_classes = 10 # MNIST total classes (0-9 digits)
-dropout = 0.75 # Dropout, probability to keep units
-
-# tf Graph input
-X = tf.placeholder(tf.float32, [None, num_input])
-Y = tf.placeholder(tf.float32, [None, num_classes])
-keep_prob = tf.placeholder(tf.float32) # dropout (keep probability)
+num_input = image_width * image_height * image_channel  # MNIST data input (img shape: 28*28)
+num_classes = 2  # MNIST total classes (0-9 digits)
+dropout = 0.75  # Dropout, probability to keep units
 
 
-def decode(serialized_example):
-  """Parses an image and label from the given `serialized_example`."""
-  features = tf.parse_single_example(
-      serialized_example,
-      # Defaults are not specified since both keys are required.
-      features={'image': tf.FixedLenFeature([], tf.string),
-               'label': tf.FixedLenFeature([], tf.int64)})
-
-  # Convert from a scalar string tensor (whose single string has
-  # length mnist.IMAGE_PIXELS) to a uint8 tensor with shape
-  # [mnist.IMAGE_PIXELS].
-  image = tf.decode_raw(features['image'], tf.float32)
-  image.set_shape((mnist.IMAGE_PIXELS))
-
-  # Convert label from a scalar uint8 tensor to an int32 scalar.
-  label = tf.cast(features['label'], tf.int32)
-
-  return image, label
+# # tf Graph input
+# X = tf.placeholder(tf.float32, [None, num_input])
+# Y = tf.placeholder(tf.float32, [None, num_classes])
+# keep_prob = tf.placeholder(tf.float32) # dropout (keep probability)
 
 
-def augment(image, label):
-  """Placeholder for data augmentation."""
-  # OPTIONAL: Could reshape into a 28x28 image and apply distortions
-  # here.  Since we are not applying any distortions in this
-  # example, and the next step expects the image to be flattened
-  # into a vector, we don't bother.
-  image = tf.reshape(image, [32, 32])
-  return image, label
+# Reading the dataset
+# 2 modes: 'file' or 'folder'
+def read_images(dataset_paths, batch_size):
+    imagepaths, labels = list(), list()
+    # An ID will be affected to each sub-folders by alphabetical order
+    total_data = 0
+    # List the directory
+    for dataset_path in dataset_paths:
+        label = 0
+        try:  # Python 2
+            classes = sorted(os.walk(dataset_path).next()[1])
+        except Exception:  # Python 3
+            classes = sorted(os.walk(dataset_path).__next__()[1])
+        # except StopIteration:
+        #     pass
+        # List each sub-directory (the classes)
+        for c in classes:
+            c_dir = os.path.join(dataset_path, c)
+            try:  # Python 2
+                walk = os.walk(c_dir).next()
+            except Exception:  # Python 3
+                walk = os.walk(c_dir).__next__()
+            # except StopIteration:
+            #     pass
+            # Add each image to the training set
+            for sample in walk[2]:
+                # Only keeps png images
+                if sample.endswith('.png'):
+                    imagepaths.append(os.path.join(c_dir, sample))
+                    labels.append(label)
+            label += 1
+
+    random.Random(seed).shuffle(labels)
+    random.Random(seed).shuffle(imagepaths)
+    num_classes = label
+    total_data = len(labels)
+
+    train_size = (int)(total_data * train_partition) - 1
+    valid_size = train_size + (int)(total_data * valid_partition) - 1
+    test_size = valid_size + (int)(total_data * test_partition) - 1
+
+    train_imagepaths = imagepaths[0:train_size]
+    train_labels = labels[0:train_size]
+
+    valid_imagepaths = imagepaths[train_size + 1:valid_size]
+    valid_labels = labels[train_size + 1:valid_size]
+
+    test_imagepaths = imagepaths[valid_size + 1:test_size]
+    test_labels = labels[valid_size + 1:test_size]
+
+    train_size = len(train_labels)
+    valid_size = len(valid_labels)
+    test_size = len(test_labels)
+    print("train set size: ", train_size)
+    print("valid set size: ", valid_size)
+    print("test set size: ", test_size)
+
+    # Convert to Tensor
+    train_imagepaths = tf.convert_to_tensor(train_imagepaths, dtype=tf.string)
+    train_labels = tf.convert_to_tensor(train_labels, dtype=tf.int32)
+
+    valid_imagepaths = tf.convert_to_tensor(valid_imagepaths, dtype=tf.string)
+    valid_labels = tf.convert_to_tensor(valid_labels, dtype=tf.int32)
+
+    test_imagepaths = tf.convert_to_tensor(test_imagepaths, dtype=tf.string)
+    test_labels = tf.convert_to_tensor(test_labels, dtype=tf.int32)
+
+    # Build a TF Queue, shuffle data
+    train_image, train_label = tf.train.slice_input_producer([train_imagepaths, train_labels],
+                                                             shuffle=True)
+
+    valid_image, valid_label = tf.train.slice_input_producer([valid_imagepaths, valid_labels],
+                                                             shuffle=True)
+
+    test_image, test_label = tf.train.slice_input_producer([test_imagepaths, test_labels],
+                                                           shuffle=True)
+
+    # Read images from disk
+    train_image = tf.read_file(train_image)
+    train_image = tf.image.decode_png(train_image, channels=image_channel)
+
+    valid_image = tf.read_file(valid_image)
+    valid_image = tf.image.decode_png(valid_image, channels=image_channel)
+
+    test_image = tf.read_file(test_image)
+    test_image = tf.image.decode_png(test_image, channels=image_channel)
+
+    # Resize images to a common size
+    train_image = tf.image.resize_images(train_image, [image_height, image_width])
+
+    valid_image = tf.image.resize_images(valid_image, [image_height, image_width])
+
+    test_image = tf.image.resize_images(test_image, [image_height, image_width])
+
+    # Normalize
+    train_image = train_image * 2.0 - 1.0
+
+    valid_image = valid_image * 2.0 - 1.0
+
+    test_image = test_image * 2.0 - 1.0
+
+    # Create batches
+    X, Y = tf.train.batch([train_image, train_label], batch_size=batch_size,
+                          capacity=batch_size * 8,
+                          num_threads=4)
+
+    Xv, Yv = tf.train.batch([valid_image, valid_label], batch_size=valid_size,
+                            capacity=batch_size * 8,
+                            num_threads=4)
+
+    Xt, Yt = tf.train.batch([test_image, test_label], batch_size=test_size,
+                            capacity=batch_size * 8,
+                            num_threads=4)
+
+    return X, Y, Xv, Yv, Xt, Yt
 
 
-def normalize(image, label):
-  """Convert `image` from [0, 255] -> [-0.5, 0.5] floats."""
-  image = tf.cast(image, tf.float32) - 0.5
-  return image, label
+# def split_dataset(images, labels, train_ratio, valid_ratio, test_ratio, shuffle=True)
+
 
 
 # Create some wrappers for simplicity
@@ -81,22 +180,31 @@ def maxpool2d(x, k=2):
 
 # Create model
 def conv_net(x, weights, biases, dropout):
-    # MNIST data input is a 1-D vector of 784 features (28*28 pixels)
     # Reshape to match picture format [Height x Width x Channel]
     # Tensor input become 4-D: [Batch Size, Height, Width, Channel]
-    x = tf.reshape(x, shape=[-1, 28, 28, 1])
+    x = tf.reshape(x, shape=[-1, image_height, image_width, image_channel])
 
     # Convolution Layer
+    # input size: 32x32x3
+    # output size: 32x32x3
     conv1 = conv2d(x, weights['wc1'], biases['bc1'])
     # Max Pooling (down-sampling)
+    # input size: 32x32x3
+    # output size: 16x16x3
     conv1 = maxpool2d(conv1, k=2)
 
     # Convolution Layer
+    # input size: 16x16x3
+    # output size: 16x16x6
     conv2 = conv2d(conv1, weights['wc2'], biases['bc2'])
     # Max Pooling (down-sampling)
+    # input size: 16x16x6
+    # output size: 8x8x6
     conv2 = maxpool2d(conv2, k=2)
 
     # Fully connected layer
+    # input size: 8x8x6
+    # output size: 1024
     # Reshape conv2 output to fit fully connected layer input
     fc1 = tf.reshape(conv2, [-1, weights['wd1'].get_shape().as_list()[0]])
     fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])
@@ -105,6 +213,8 @@ def conv_net(x, weights, biases, dropout):
     fc1 = tf.nn.dropout(fc1, dropout)
 
     # Output, class prediction
+    # input size: 1024
+    # output size: 2
     out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])
     return out
 
@@ -112,64 +222,82 @@ def conv_net(x, weights, biases, dropout):
 # Store layers weight & bias
 weights = {
     # 5x5 conv, 1 input, 32 outputs
-    'wc1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
+    'wc1': tf.Variable(tf.random_normal([5, 5, image_channel, 6])),
     # 5x5 conv, 32 inputs, 64 outputs
-    'wc2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
+    'wc2': tf.Variable(tf.random_normal([5, 5, 6, 12])),
     # fully connected, 7*7*64 inputs, 1024 outputs
-    'wd1': tf.Variable(tf.random_normal([7*7*64, 1024])),
+    'wd1': tf.Variable(tf.random_normal([8 * 8 * 12, 256])),
     # 1024 inputs, 10 outputs (class prediction)
-    'out': tf.Variable(tf.random_normal([1024, num_classes]))
+    'out': tf.Variable(tf.random_normal([256, num_classes]))
 }
 
 biases = {
-    'bc1': tf.Variable(tf.random_normal([32])),
-    'bc2': tf.Variable(tf.random_normal([64])),
-    'bd1': tf.Variable(tf.random_normal([1024])),
+    'bc1': tf.Variable(tf.random_normal([6])),
+    'bc2': tf.Variable(tf.random_normal([12])),
+    'bd1': tf.Variable(tf.random_normal([256])),
     'out': tf.Variable(tf.random_normal([num_classes]))
 }
 
+X, Y, Xv, Yv, Xt, Yt = read_images(image_path, batch_size)
+
 # Construct model
-logits = conv_net(X, weights, biases, keep_prob)
-prediction = tf.nn.softmax(logits)
+logits = conv_net(X, weights, biases, dropout)
+prediction = tf.nn.softmax(conv_net(Xv, weights, biases, dropout=1.0))
+prediction_t = tf.nn.softmax(conv_net(Xt, weights, biases, dropout=1.0))
 
 # Define loss and optimizer
-loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(
+loss_op = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
     logits=logits, labels=Y))
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
 train_op = optimizer.minimize(loss_op)
 
-
 # Evaluate model
-correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(Y, 1))
+correct_pred = tf.equal(tf.argmax(prediction, 1), tf.cast(Yv, tf.int64))
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+correct_pred_t = tf.equal(tf.argmax(prediction_t, 1), tf.cast(Yt, tf.int64))
+accuracy_t = tf.reduce_mean(tf.cast(correct_pred_t, tf.float32))
+
 
 # Initialize the variables (i.e. assign their default value)
 init = tf.global_variables_initializer()
 
+saver = tf.train.Saver()
 
 # Start training
 with tf.Session() as sess:
-
     # Run the initializer
     sess.run(init)
 
-    for step in range(1, num_steps+1):
-        batch_x, batch_y = mnist.train.next_batch(batch_size)
+    tf.train.start_queue_runners()
+    max_acc = 0.0
+    acc = 0.0
+    for step in range(1, num_steps + 1):
+        # batch_x, batch_y = mnist.train.next_batch(batch_size)
         # Run optimization op (backprop)
-        sess.run(train_op, feed_dict={X: batch_x, Y: batch_y, keep_prob: dropout})
+        # sess.run(train_op, feed_dict={X: batch_x, Y: batch_y, keep_prob: dropout})
+
         if step % display_step == 0 or step == 1:
             # Calculate batch loss and accuracy
-            loss, acc = sess.run([loss_op, accuracy], feed_dict={X: batch_x,
-                                                                 Y: batch_y,
-                                                                 keep_prob: 1.0})
-            print("Step " + str(step) + ", Minibatch Loss= " + \
-                  "{:.4f}".format(loss) + ", Training Accuracy= " + \
+            _, loss, acc = sess.run([train_op, loss_op, accuracy])
+            print("Step " + str(step) + ", Minibatch Loss= " +
+                  "{:.4f}".format(loss) + ", Training Accuracy= " +
                   "{:.3f}".format(acc))
+            if max_acc < acc:
+                max_acc = acc
+                if max_acc > 0.95:
+                    saver.save(sess, './my_tf_model')
+        else:
+            sess.run(train_op)
 
     print("Optimization Finished!")
 
+    print("Testing Accuracy:",
+          sess.run(accuracy_t))
+    print("Saved")
+
     # Calculate accuracy for 256 MNIST test images
-    print("Testing Accuracy:", \
-        sess.run(accuracy, feed_dict={X: mnist.test.images[:256],
-                                      Y: mnist.test.labels[:256],
-                                      keep_prob: 1.0}))
+    # print("Testing Accuracy:", \
+    #     sess.run(accuracy, feed_dict={X: mnist.test.images[:256],
+    #                                   Y: mnist.test.labels[:256],
+    #                                   keep_prob: 1.0}))
